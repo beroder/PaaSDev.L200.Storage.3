47439714f443c5c193da17d0ce5b300fae22583a
beroder <beroder@hotmail.com>
2018-05-26 11:24:57 -0400

    Removing deployment script

--- DeployStorage.ps1
+++
@@ -1,9 +0,0 @@
-$resourcegroupname = "PaaSDev.L200.Storage.3"
-
-Write-Host "Logging in..."
-Login-AzureRmAccount
-
-Write-Host "Creating resource group..."
-New-AzureRmResourceGroup -Name $resourcegroupname -Location "East US" -Force
-Write-Host "Deploying storage account..."
-New-AzureRmResourceGroupDeployment -Name $resourcegroupname -ResourceGroupName $resourcegroupname -TemplateFile .\storage.json
